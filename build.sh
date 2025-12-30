12761b682ba9c4331f99a528fb79b121b45a7855
Johan Walles <johan.walles@gmail.com>
2025-12-30 13:22:50 +0100

    Set up build environment

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+if [ -z ${CI+x} ]; then
+    # Local build, not in CI, format source
+    gofmt -s -w .
+fi
+
+VERSION="$(git describe --tags --dirty --always)"
+
+BINARY="ptop"
+if [ -n "${GOOS}${GOARCH}" ]; then
+    EXE=""
+    if [ "${GOOS}" = "windows" ]; then
+        EXE=".exe"
+    fi
+    BINARY="releases/${BINARY}-${VERSION}-${GOOS}-${GOARCH}${EXE}"
+fi
+
+# Linker flags version number trick below from here:
+# https://www.reddit.com/r/golang/comments/4cpi2y/question_where_to_keep_the_version_number_of_a_go/d1kbap7?utm_source=share&utm_medium=web2x
+
+# Linker flags -s and -w strips debug data, but keeps whatever is needed for
+# proper panic backtraces, this makes binaries smaller:
+# https://boyter.org/posts/trimming-golang-binary-fat/
+
+# This line must be last in the script so that its return code
+# propagates properly to its caller
+#
+# Note that ${RACE} must *not* be quoted, we want it to disappear if empty.
+# shellcheck disable=SC2086
+go build ${RACE} -trimpath -ldflags="-s -w -X main.versionString=${VERSION}" -o "${BINARY}" ./cmd/ptop
+
+# Alternative build line, if you want to attach to the running process in the Go debugger:
+# go build -ldflags="-X main.versionString=${VERSION}" -gcflags="all=-N -l" -o "${BINARY}" ./cmd/ptop
