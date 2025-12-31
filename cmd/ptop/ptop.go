d554dee207229e5d347479d07671190081b7093c
Johan Walles <johan.walles@gmail.com>
2025-12-31 08:12:36 +0100

    Animate the process listing

--- cmd/ptop/ptop.go
+++ cmd/ptop/ptop.go
@@ -3,28 +3,41 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/walles/moor/v2/twin"
 	"github.com/walles/ptop/internal/processes"
 )
 
 func main() {
-	allProcesses, err := processes.GetAll()
-	if err != nil {
-		fmt.Println("Error retrieving processes:", err)
-		os.Exit(1)
-	}
-
 	screen, err := twin.NewScreen()
 	if err != nil {
 		fmt.Println("Error creating screen:", err)
 		os.Exit(1)
 	}
 
-	processes.Render(processes.ByCpuPercent(allProcesses), screen)
+	events := make(chan twin.Event)
+	go func() {
+		for event := range screen.Events() {
+			events <- event
+		}
+	}()
+	go func() {
+		for tick := range time.NewTicker(time.Second).C {
+			events <- tick
+		}
+	}()
 
 	for {
-		event := <-screen.Events()
+		allProcesses, err := processes.GetAll()
+		if err != nil {
+			fmt.Println("Error retrieving processes:", err)
+			os.Exit(1)
+		}
+
+		processes.Render(processes.ByCpuPercent(allProcesses), screen)
+
+		event := <-events
 
 		if event, ok := event.(twin.EventRune); ok {
 			if event.Rune() == 'q' {
