7513f7be9dee13a05ceb3af01f2b019fca413ff4
Johan Walles <johan.walles@gmail.com>
2026-01-02 16:20:21 +0100

    Bail on assertion failures

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -9,7 +9,7 @@ func Equal[V comparable](t *testing.T, actual, expected V) {
 	t.Helper()
 
 	if expected != actual {
-		t.Errorf(
+		t.Fatalf(
 			"\nExpected: %v\nActual:   %v", expected, actual)
 	}
 }
@@ -18,7 +18,7 @@ func SliceEqual[V comparable](t *testing.T, actual, expected []V) {
 	t.Helper()
 
 	if !reflect.DeepEqual(expected, actual) {
-		t.Errorf(
+		t.Fatalf(
 			"\nExpected: %v\nActual:   %v", expected, actual)
 	}
 }
