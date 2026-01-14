01f2e16270edf69b93250f6634553dfa57c534a7
Johan Walles <johan.walles@gmail.com>
2026-01-14 21:47:49 +0100

    Enable making multiple themes

--- internal/themes/theme.go
+++ internal/themes/theme.go
@@ -5,61 +5,97 @@ import "github.com/walles/moor/v2/twin"
 // Hard coded dark theme
 type Theme struct {
 	bg *twin.Color
+	loadBarMin      twin.Color
+	loadBarMaxRam   twin.Color
+	loadBarMaxCpu   twin.Color
+	loadBarMaxIO    twin.Color
+	border          twin.Color
+	borderTitle     twin.Color
+	top             twin.Color
+	bottom          twin.Color
+	backgroundColor twin.Color
+	loadLow         twin.Color
+	loadMedium      twin.Color
+	loadHigh        twin.Color
 }
 
 // NOTE: Use some online OKLCH color picker for experimenting with colors
 
 func NewDarkTheme(bg *twin.Color) Theme {
-	return Theme{bg: bg}
+	var background twin.Color
+	if bg != nil {
+		background = *bg
+	} else {
+		background = twin.NewColorHex(0x000000)
+	}
+
+	top := twin.NewColorHex(0xdddddd)
+	bottom := top.Mix(background, 0.5)
+
+	return Theme{
+		bg:               bg,
+		loadBarMin:       twin.NewColorHex(0x000000),
+		loadBarMaxRam:    twin.NewColorHex(0x2020ff),
+		loadBarMaxCpu:    twin.NewColorHex(0x801020),
+		loadBarMaxIO:     twin.NewColorHex(0xd0d020),
+		border:           twin.NewColorHex(0x7070a0),
+		borderTitle:      twin.NewColorHex(0xffc0c0),
+		top:              top,
+		bottom:           bottom,
+		backgroundColor:  background,
+		loadLow:          twin.NewColorHex(0x00ff00),
+		loadMedium:       twin.NewColorHex(0xffff00),
+		loadHigh:         twin.NewColorHex(0xff0000),
+	}
 }
 
 func (t Theme) LoadBarMin() twin.Color {
-	return twin.NewColorHex(0x000000)
+	return t.loadBarMin
 }
 
 func (t Theme) LoadBarMaxRam() twin.Color {
-	return twin.NewColorHex(0x2020ff)
+	return t.loadBarMaxRam
 }
 
 func (t Theme) LoadBarMaxCpu() twin.Color {
-	return twin.NewColorHex(0x801020)
+	return t.loadBarMaxCpu
 }
 
 func (t Theme) LoadBarMaxIO() twin.Color {
-	return twin.NewColorHex(0xd0d020)
+	return t.loadBarMaxIO
 }
 
 func (t Theme) Border() twin.Color {
-	return twin.NewColorHex(0x7070a0)
+	return t.border
 }
 
 func (t Theme) BorderTitle() twin.Color {
-	return twin.NewColorHex(0xffc0c0)
+	return t.borderTitle
 }
 
 func (t Theme) Top() twin.Color {
-	return twin.NewColorHex(0xdddddd)
+	return t.top
 }
 
 func (t Theme) Bottom() twin.Color {
-	return t.Top().Mix(t.Background(), 0.5)
+	return t.bottom
 }
 
 func (t Theme) Background() twin.Color {
 	if t.bg != nil {
 		return *t.bg
 	}
-	return twin.NewColorHex(0x000000)
+	return t.backgroundColor
 }
 
 func (t Theme) LoadLow() twin.Color {
-	return twin.NewColorHex(0x00ff00)
+	return t.loadLow
 }
 
 func (t Theme) LoadMedium() twin.Color {
-	return twin.NewColorHex(0xffff00)
+	return t.loadMedium
 }
 
 func (t Theme) LoadHigh() twin.Color {
-	return twin.NewColorHex(0xff0000)
+	return t.loadHigh
 }
