5fcd174fa16fce184c085d61898670ac1174d063
Johan Walles <johan.walles@gmail.com>
2026-01-21 06:45:38 +0100

    Simplify too-small-window code

--- internal/ptop/rendertoosmallscreen.go
+++ internal/ptop/rendertoosmallscreen.go
@@ -21,17 +21,10 @@ func renderTooSmallScreen(screen twin.Screen, theme themes.Theme) {
 	}
 	x := (width - len(message)) / 2
 
-	halfWayY := height / 2
-	topToMiddleTextRamp := ui.NewColorRamp(0, float64(halfWayY), theme.Background(), theme.Foreground())
-	middleToBottomTextRamp := ui.NewColorRamp(float64(halfWayY+1), float64(height), theme.Foreground(), theme.Background())
+	topBottomTextRamp := ui.NewColorRamp(0, float64(height-1), theme.Background(), theme.Foreground(), theme.Background())
 
 	for y := range height {
-		var color twin.Color
-		if y <= halfWayY {
-			color = topToMiddleTextRamp.AtInt(y)
-		} else {
-			color = middleToBottomTextRamp.AtInt(y)
-		}
+		color := topBottomTextRamp.AtInt(y)
 		drawText(screen, x, y, width, message, twin.StyleDefault.WithForeground(color))
 	}
 
@@ -39,16 +32,11 @@ func renderTooSmallScreen(screen twin.Screen, theme themes.Theme) {
 
 	if width < minWidth {
 		// Red left and right borders
-		topMiddleRamp := ui.NewColorRamp(0, float64(halfWayY), theme.Background(), red)
-		middleBottomRamp := ui.NewColorRamp(float64(halfWayY+1), float64(height-1), red, theme.Background())
+		topBottomRamp := ui.NewColorRamp(0, float64(height-1), theme.Background(), red, theme.Background())
 
 		for y := range height {
-			var color twin.Color
-			if y <= halfWayY {
-				color = topMiddleRamp.AtInt(y)
-			} else {
-				color = middleBottomRamp.AtInt(y)
-			}
+			color := topBottomRamp.AtInt(y)
+
 			screen.SetCell(0, y, twin.StyledRune{
 				Style: twin.StyleDefault.WithForeground(color),
 				Rune:  '<',
@@ -62,16 +50,11 @@ func renderTooSmallScreen(screen twin.Screen, theme themes.Theme) {
 
 	if height < minHeight {
 		// Red top and bottom borders
-		leftMiddleRamp := ui.NewColorRamp(0, float64(width)/2, theme.Background(), red)
-		middleRightRamp := ui.NewColorRamp(float64(width)/2+1, float64(width-1), red, theme.Background())
+		topBottomRamp := ui.NewColorRamp(0, float64(width-1), theme.Background(), red, theme.Background())
 
 		for x := range width {
-			var color twin.Color
-			if x <= width/2 {
-				color = leftMiddleRamp.AtInt(x)
-			} else {
-				color = middleRightRamp.AtInt(x)
-			}
+			color := topBottomRamp.AtInt(x)
+
 			screen.SetCell(x, 0, twin.StyledRune{
 				Style: twin.StyleDefault.WithForeground(color),
 				Rune:  '^',
