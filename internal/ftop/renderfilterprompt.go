8b307ceb32c53613ddcdca864914aa3206621487
Johan Walles <johan.walles@gmail.com>
2026-02-11 19:08:15 +0100

    Prep for showing filter string

--- internal/ftop/renderfilterprompt.go
+++ internal/ftop/renderfilterprompt.go
@@ -1,25 +1,24 @@
 package ftop
 
 import (
-	"github.com/walles/ftop/internal/themes"
 	"github.com/walles/moor/v2/twin"
 )
 
-func renderFilterPrompt(screen twin.Screen, theme themes.Theme, active bool, x0 int, y int, x1 int) {
+func (ui *Ui) renderFilterPrompt(active bool, x0 int, y int, x1 int) {
 	x := x0
 
 	if active {
-		x += screen.SetCell(x, y, twin.NewStyledRune('F', twin.StyleDefault.WithForeground(theme.Foreground()).WithAttr(twin.AttrReverse)))
-		x += drawText(screen, x, y, x1, "ilter", twin.StyleDefault.WithForeground(theme.Foreground()).WithAttr(twin.AttrDim).WithAttr(twin.AttrUnderline))
-		screen.SetCell(x, y, twin.StyledRune{
-			Style: twin.StyleDefault.WithForeground(theme.HighlightedForeground()).WithAttr(twin.AttrBold),
+		x += ui.screen.SetCell(x, y, twin.NewStyledRune('F', twin.StyleDefault.WithForeground(ui.theme.Foreground()).WithAttr(twin.AttrReverse)))
+		x += drawText(ui.screen, x, y, x1, "ilter", twin.StyleDefault.WithForeground(ui.theme.Foreground()).WithAttr(twin.AttrDim).WithAttr(twin.AttrUnderline))
+		ui.screen.SetCell(x, y, twin.StyledRune{
+			Style: twin.StyleDefault.WithForeground(ui.theme.HighlightedForeground()).WithAttr(twin.AttrBold),
 			Rune:  '⏎',
 		})
 	} else {
-		x += screen.SetCell(x, y, twin.StyledRune{
-			Style: twin.StyleDefault.WithForeground(theme.HighlightedForeground()),
+		x += ui.screen.SetCell(x, y, twin.StyledRune{
+			Style: twin.StyleDefault.WithForeground(ui.theme.HighlightedForeground()),
 			Rune:  'F',
 		})
-		drawText(screen, x, y, x1, "ilter", twin.StyleDefault.WithForeground(theme.Foreground()).WithAttr(twin.AttrDim))
+		drawText(ui.screen, x, y, x1, "ilter", twin.StyleDefault.WithForeground(ui.theme.Foreground()).WithAttr(twin.AttrDim))
 	}
 }
