291b61edfa3e7faea367c9e98631b8d0983c4b3d
Johan Walles <johan.walles@gmail.com>
2026-02-21 00:21:31 +0100

    Don't kill process unless confirmed

--- internal/ftop/eventhandler-kill.go
+++ internal/ftop/eventhandler-kill.go
@@ -1,6 +1,10 @@
 package ftop
 
 import (
+	"os"
+	"syscall"
+
+	"github.com/walles/ftop/internal/log"
 	"github.com/walles/ftop/internal/processes"
 	"github.com/walles/moor/v2/twin"
 )
@@ -11,7 +15,38 @@ type eventHandlerKill struct {
 }
 
 func (h *eventHandlerKill) onRune(r rune) {
-	// FIXME
+	defer func() {
+		// After handling the kill, switch back to the base event handler
+		h.ui.eventHandler = &eventHandlerBase{ui: h.ui}
+	}()
+
+	if r != 'k' {
+		// Abort
+		return
+	}
+
+	if h.ui.pickedProcess == nil {
+		// Process not available
+		log.Infof("No process selected, cannot kill")
+		return
+	}
+
+	// Kill the process
+
+	p, err := os.FindProcess(h.ui.pickedProcess.Pid)
+	if err != nil {
+		log.Infof("Process %s not found for killing: %v", h.ui.pickedProcess.String(), err)
+		return
+	}
+
+	err = p.Signal(syscall.SIGKILL)
+	if err != nil {
+		log.Infof("Failed to kill process %s: %v", h.ui.pickedProcess.String(), err)
+		return
+	}
+
+	log.Debugf("Killed process %s", h.ui.pickedProcess.String())
+
 }
 
 func (h *eventHandlerKill) onKeyCode(keyCode twin.KeyCode) {
