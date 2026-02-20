be372a2b520b8644a036513dc3a307123d8ba2c6
Johan Walles <johan.walles@gmail.com>
2026-02-21 00:13:11 +0100

    Draw a kill prompt

--- internal/ftop/renderkillui.go
+++ internal/ftop/renderkillui.go
@@ -1,6 +1,10 @@
 package ftop
 
-import "github.com/walles/moor/v2/twin"
+import (
+	"fmt"
+
+	"github.com/walles/moor/v2/twin"
+)
 
 func (u *Ui) renderKillUi(width int, height int) {
 	w, h := u.screen.Size()
@@ -11,16 +15,30 @@ func (u *Ui) renderKillUi(width int, height int) {
 	y0 := yCenter - height/2
 	y1 := yCenter + height/2
 
-	FIXME: Add a question to this dialog. Consider line lengths and wrapping.
-
 	for x := x0; x <= x1; x++ {
 		for y := y0; y <= y1; y++ {
-			u.screen.SetCell(x, y, twin.StyledRune{
-				Rune:  ' ',
-				Style: twin.StyleDefault.WithBackground(u.theme.Foreground()),
-			})
+			u.screen.SetCell(x, y, twin.StyledRune{Rune: ' '})
 		}
 	}
+
+	killHandler, ok := u.eventHandler.(*eventHandlerKill)
+	if !ok {
+		panic(fmt.Sprintf("Not a kill handler: %+v", u.eventHandler))
+	}
+
+	// "Press k to kill launchd(1)."
+	x := x0 + 1
+	x += drawText(u.screen, x, y0+1, x1, "Press ", u.theme.PromptActive())
+	x += drawText(u.screen, x, y0+1, x1, "k", u.theme.PromptKey())
+	x += drawText(u.screen, x, y0+1, x1, " to kill ", u.theme.PromptActive())
+	x += drawText(u.screen, x, y0+1, x1,
+		killHandler.process.String(),
+		twin.StyleDefault.WithForeground(u.theme.HighlightedForeground()),
+	)
+	u.screen.SetCell(x, y0+1, twin.StyledRune{
+		Rune:  '.',
+		Style: u.theme.PromptActive(),
+	})
 
 	renderFrame(u.screen, u.theme, x0, y0, x1, y1, "Kill process")
 }
