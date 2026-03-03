ef1ebef6c1cd6fd01159f77ad943afa6c77b3fae
Johan Walles <johan.walles@gmail.com>
2026-03-03 18:34:00 +0100

    Tune hierarchy color scheme

--- internal/ftop/renderprocessinfopane.go
+++ internal/ftop/renderprocessinfopane.go
@@ -25,8 +25,8 @@ func (u *Ui) renderProcessInfoPane(y0, y1 int) {
 		bottomUpNames = append(bottomUpNames, p.Command)
 	}
 
-	// Always show the current process as highlighted. Before that, go from faded to foreground.
-	ramp := ui.NewColorRamp(0, float64(len(bottomUpNames))-2, u.theme.Foreground(), u.theme.FadedForeground())
+	// Always show the current process as highlighted. Before that, go from faded to almost-foreground.
+	ramp := ui.NewColorRamp(0, float64(len(bottomUpNames))-1, u.theme.Foreground(), u.theme.FadedForeground())
 
 	highlighted := twin.StyleDefault.WithForeground(u.theme.HighlightedForeground())
 	plain := twin.StyleDefault.WithForeground(u.theme.Foreground())
@@ -39,16 +39,16 @@ func (u *Ui) renderProcessInfoPane(y0, y1 int) {
 			hierarchy = append(hierarchy, twin.StyledRune{Rune: '―', Style: style})
 		}
 
-		name := bottomUpNames[i]
 		var color twin.Color
 		if i == 0 {
 			// Always highlight the current process
 			color = u.theme.HighlightedForeground()
 		} else {
-			color = ramp.AtInt(i - 1)
+			color = ramp.AtInt(i)
 		}
 		style := twin.StyleDefault.WithForeground(color)
 
+		name := bottomUpNames[i]
 		for _, r := range name {
 			hierarchy = append(hierarchy, twin.StyledRune{Rune: r, Style: style})
 		}
